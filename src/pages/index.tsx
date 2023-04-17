2a6fb081111908e2b312512fbdf0a13143bbe15d
Pawel Majewski <pawel.majewski@tsh.io>
2023-04-17 15:11:50 +0200

    create pull-to-fetch.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { motion } from 'framer-motion';
 
 export default function Home() {
@@ -18,7 +17,7 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <motion.a
-          href="./drawer"
+          href="./pull-to-fetch"
           whileHover={{
             scale: 1.2,
             transition: { duration: 0.35 },
@@ -27,13 +26,13 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4"
         >
           <h2 className={'mb-3 text-2xl font-semibold'}>
-            Drawer example{' '}
+            Pull to fetch example{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Example of drawer that is being opened by clicking button. Mount/Unmount animation with AnimatePresence
+            Example of pull to fetch. Using gesture handlers and useSpring/useTransition hooks
           </p>
         </motion.a>
       </div>
---
+++ src/pages/pull-to-fetch.tsx
@@ -0,0 +1,109 @@
+import { DraggableProps, motion, useTransform, useAnimation, useSpring, AnimationProps } from 'framer-motion';
+import { useState } from 'react';
+
+export default function PullToFetchPage() {
+  const scrollY = useSpring(0, { stiffness: 500, damping: 50 }); // spring with stiffness and damping and our base value on which we will apply transforms
+
+  const opacity = useTransform(scrollY, [0, 30], [0, 1]); // opacity will be 0 when scrollY is 0 and 1 when scrollY is 30
+  const loaderY = useTransform(scrollY, [0, 30], [-48, 0]); // loaderY will be 0 when scrollY is 0 and -48 when scrollY is 30
+
+  const dragConstraints: DraggableProps['dragConstraints'] = {
+    // dragConstraints for respecting the boundaries of the parent
+    top: 0,
+    bottom: 0,
+  };
+
+  const dragElastic: DraggableProps['dragElastic'] = {
+    // dragElastic for elastic effect when we drag the element. It's set by default to every direction, but we don't want it to be elastic to the top
+    bottom: 0.15,
+  };
+
+  const loadingAnimation = useAnimation(); // animation handler for the loader spinner
+
+  const animationVariants: AnimationProps['variants'] = {
+    // set of animation variants for the loader spinner
+    startLoading: {
+      rotate: 360,
+      transition: {
+        repeat: Infinity,
+        ease: 'linear',
+        duration: 1,
+      },
+    },
+    endLoading: {
+      rotate: 0,
+    },
+  };
+
+  const [randomMemeIndex, setRandomMemeIndex] = useState(0);
+  const memes = [
+    'https://i.redd.it/254twe19ewra1.jpg',
+    'https://i.redd.it/yujh8gzbcypa1.png',
+    'https://i.redd.it/i1zsudixvipa1.jpg',
+  ];
+
+  const onDragEnd = () => {
+    const y = scrollY.get();
+
+    if (y > 30) {
+      scrollY.set(30);
+      scrollY.stop();
+      loadingAnimation.start('startLoading');
+
+      setTimeout(() => {
+        setRandomMemeIndex((prev) => (prev + 1) % memes.length);
+        loadingAnimation.start('endLoading');
+        scrollY.set(0);
+      }, 2000);
+    }
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-cebter p-24 bg-gray-800">
+      <h1 className="text-4xl font-bold mb-8">Pull to fetch</h1>
+      <motion.div className="w-96 h-[700px] border-[12px] border-black text-black rounded-[32px] bg-gray-200 p-2 overflow-hidden relative">
+        <motion.span
+          className="w-8 h-8 rounded-full bg-gray-400 text-white flex items-center justify-center absolute top-4 mx-auto left-0 right-0"
+          style={{
+            opacity,
+            y: loaderY,
+          }}
+        >
+          <motion.svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-5 h-5"
+            variants={animationVariants}
+            animate={loadingAnimation}
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+            />
+          </motion.svg>
+        </motion.span>
+        <motion.div
+          className="flex flex-col items-center gap-4 py-4 min-h-full"
+          style={{
+            y: scrollY, // apply the scrollY transform to the y property, this way we can control it two ways: by dragging and by setting/editing the value
+          }}
+          drag="y"
+          dragConstraints={dragConstraints}
+          dragElastic={dragElastic}
+          onDragEnd={onDragEnd}
+        >
+          <motion.h2>Pull to fetch another meme</motion.h2>
+          <motion.img
+            className="max-w-full max-h-[75%] object-contain"
+            src={memes[randomMemeIndex]}
+            draggable={false}
+          />
+        </motion.div>
+      </motion.div>
+    </main>
+  );
+}
